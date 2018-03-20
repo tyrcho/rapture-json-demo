2451804939c6a64ca7a01a771ef5f49646a7501a
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-03-20 12:38:35 +0100

    :truck: Moved rapture 1.1 demo

--- rapture1.1/src/main/scala/BiggerDemo.scala
+++ rapture1.1/src/main/scala/BiggerDemo.scala
@@ -1,10 +1,10 @@
-import rapture._
-import rapture.data.Serializer
+import rapture.data.{Extractor, Serializer}
 import rapture.json._
 import rapture.json.jsonBackends.jawn._
 
 object BiggerDemo extends App {
-//  implicit val ser = implicitly[Serializer[Endpoint,Json]]
+implicit val ser = implicitly[Serializer[Endpoint,Json]]
+implicit val ex = implicitly[Extractor[Endpoint,Json]]
 
   val problem = Problem(0, 0)
   val js = Json(problem)
@@ -28,6 +28,7 @@ case class Problem(
 
 case class Video(id: Int, size: Int)
 
-case class Endpoint(id: Int, latency: Int, serverLatencies: Map[Int, Int])
+case class Endpoint(id: Int, latency: Int, serverLatencies: Map[String, Int])
+// does not work if the keys of the Map are Int instead of String
 
 case class Request(videoId: Int, endpointId: Int, count: Int)
